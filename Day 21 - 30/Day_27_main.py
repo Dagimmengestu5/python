59deec94e2eba26f62fa6bc35ca30b83140513a3
D <dagimmengestu5@gmail.com>
2025-03-21 16:09:52 +0300

    day 27 test

--- Day 21 - 30/Day_27_main.py
+++ Day 21 - 30/Day_27_main.py
@@ -1,10 +1,21 @@
-import tkinter
-windows = tkinter.Tk()
+from tkinter import *
+windows = Tk()
 windows.title("my first GUI")
 windows.minsize(width=500, height=400)
-windows.label()
+word = Label(text="This is the bast of my life")
+word.pack(side="top")
+# word["text"] = "new text"
+word.config(text="hi their")
 
+def click():
+    print("yes this is work")
+    new = Input.get()
+    word.config(text=new)
 
+Input =Entry(width=30 )
+Input.pack()
+button = Button(text="click me", command=click)
+button.pack()
 
 
 
