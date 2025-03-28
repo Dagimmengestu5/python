24a7adebf51ffb5278ad614b28efd84a5a0fddcb
D <dagimmengestu5@gmail.com>
2025-03-28 09:55:53 +0300

    day 33 start

--- Day 31 - 40/Day_33.py
+++ Day 31 - 40/Day_33.py
@@ -1,12 +1,47 @@
+import smtplib
 import requests
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-latitude = data["iss_position"]["latitude"]
-longtude = data["iss_position"]["longitude"]
-iss_position = (latitude, longtude)
-print(iss_position)
-print(data)
+from datetime import datetime
+import time
+
+
+my_email = "daveman191919@gmail.com"
+my_password = "eajnrvqbekykqdvy"
+my_lat = 8.980603
+my_long = 38.757759
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if my_lat - 5 <= iss_latitude <= my_lat + 5 and my_long - 5 <= iss_longitude <= my_long + 5:
+        return True
+def is_night():
+    parameters = {
+        "lat":my_lat,
+        "lng":my_long,
+        "formatted": 0
+    }
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com")as connection:
+            connection.starttls()
+            connection.login(user=my_email,password=my_password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="dagimmengestu5@gmail.com",
+                                msg="subject:It's the day of the week☝️☝️☝️☝️☝️☝️\n\niss is above you")
 
 
 # go to up on the sky ok
