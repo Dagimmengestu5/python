688511bd370e72397373dbc81b0ecb19a753a7c0
D <dagimmengestu5@gmail.com>
2025-03-28 09:56:53 +0300

    day 33 start

--- Day 31 - 40/Day_33.py
+++ Day 31 - 40/Day_33.py
@@ -41,7 +41,7 @@ while True:
             connection.login(user=my_email,password=my_password)
             connection.sendmail(from_addr=my_email,
                                 to_addrs="dagimmengestu5@gmail.com",
-                                msg="subject:It's the day of the week☝️☝️☝️☝️☝️☝️\n\niss is above you")
+                                msg="subject:It's the day of the week☝️☝️☝️☝️☝️\n\niss is above you")
 
 
 # go to up on the sky ok
