3811c05db9396a34d519e288e4f1201548a5fcb8
D <dagimmengestu5@gmail.com>
2025-03-29 12:27:36 +0300

    day 35 start

--- Day 31 - 40/Day_35.py
+++ Day 31 - 40/Day_35.py
@@ -1,2 +1,15 @@
+import requests
+
+
+
+owm_endpoint = "http://api.openweathermap.org/data/2.5/onecall"
 api_key = "860c0060a78cf87e04e6fbb4d9901fde"
+parameters = {
+    "lat":
+    "lon":
+    "appid": api_key
+}
+
+
+requests.get(url=owm_endpoint, params=)
 
