4a3a624fad4b7a11b97dec124383fb464d3c04ad
D <dagimmengestu5@gmail.com>
2025-03-31 16:34:07 +0300

    day 35 start

--- Day 31 - 40/Day_35.py
+++ Day 31 - 40/Day_35.py
@@ -3,16 +3,17 @@ import requests
 
 
 owm_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = "694ca15c7440a3505d402bc51b4e3b89"
+api_key = "bb5ccdd367b9aa706aa945f5ba0f9045"
 parameters = {
-    "lat":8.980603,
-    "lon": 38.757759,
-    "appid": api_key,
+    "lat":25.325665,
+    "lon": -20.321556,
+    "appid": "bb5ccdd367b9aa706aa945f5ba0f9045",
     "exclude":"current,minutely,daily"
 }
 
 
 response = requests.get(url=owm_endpoint, params=parameters)
 # response.raise_for_status()
-print(response.json())
+whether = response.json()
+print(whether)
 
