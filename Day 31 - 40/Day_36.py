55f04167406ac30720f6479d615a02b0517a0f37
D <dagimmengestu5@gmail.com>
2025-04-02 08:38:29 +0300

    Day 36 start

--- Day 31 - 40/Day_36.py
+++ Day 31 - 40/Day_36.py
@@ -1,6 +1,18 @@
+import requests
+
+
 stock_name = "BNB"
-company_name = "droga"
+company_name = "bitget"
 
 stock_endpoint = "https://www.alphavantage.co/query"
 news_endpoint = "https://newsapi.org/v2/everything"
 stock_api_key = "5U39QU3VE5WJN05T"
+
+stock_parameters = {
+    "function":"TIME_SERIES_INTRADAY",
+    "symbol":stock_name,
+    "interval":"1 min",
+    "apikey":stock_api_key
+}
+response = requests.get(stock_endpoint, params=stock_parameters)
+print(response.json())
