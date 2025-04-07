f4709a191273711520c568f1ab5110c4187f7d32
D <dagimmengestu5@gmail.com>
2025-04-07 13:48:34 +0300

    Day 39 start

--- Day 31 - 40/Day_38.py
+++ Day 31 - 40/Day_38.py
@@ -1,44 +1,4 @@
-# import requests
-# from datetime import datetime
-#
-#
-#
-# today = datetime.now()
-# # print(today.strftime("%Y%m%d"))
-#
-#
-# GENDER = "Male"
-# WEIGHT_KG = "60"
-# HEIGHT_CM = "130"
-# AGE = "21"
-#
-# APP_ID = "c22e45fa"
-# API_KEY = "cd92e465c4aacd825b7787bb5aa5cc40"
-#
-# exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-# sheet_endpoint = "https://docs.google.com/spreadsheets/d/1xMOT25uLpdscKqqVlFzuwF-v8Bq8-DOKgjzlZjWa8HU/edit?usp=sharing"
-#
-# exercise_text = input("Tell me which exercises you did: ")
-#
-# headers = {
-#     "x-app-id": APP_ID,
-#     "x-app-key": API_KEY,
-# }
-#
-#
-# parameters = {
-#     "query": exercise_text,
-#     "gender": GENDER,
-#     "weight_kg": WEIGHT_KG,
-#     "height_cm": HEIGHT_CM,
-#     "age": AGE
-# }
-#
-# response = requests.post(exercise_endpoint, json=parameters, headers=headers)
-# result = response.json()
-# print(result)
-#
-#
+
 
 import requests
 from datetime import datetime
@@ -49,12 +9,11 @@ WEIGHT_KG = 60
 HEIGHT_CM = 150
 AGE = 22
 
-sheet_endpoint = "https://docs.google.com/spreadsheets/d/1xMOT25uLpdscKqqVlFzuwF-v8Bq8-DOKgjzlZjWa8HU/edit?usp=sharing"
-
-APP_ID = "c22e45fa"
+APP_ID = 'c22e45fa'
 API_KEY = "cd92e465c4aacd825b7787bb5aa5cc40"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "1OVR03qEdWmybLi5bUl9P_wbPXWmIDbquYj7tDIb59e4"
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -75,7 +34,7 @@ response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 print(result)
 
-today_date = datetime.now().strftime("%Y%m%d")
+today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
 for exercise in result["exercises"]:
@@ -89,22 +48,23 @@ for exercise in result["exercises"]:
         }
     }
 
-    # No Auth
-    # sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    #No Auth
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
 
-    # Basic Auth
-    # sheet_response = requests.post(
-    #     sheet_endpoint,
-    #     json=sheet_inputs,
-    #     auth=(
-    #         os.environ["USERNAME"],
-    #         os.environ["PASSWORD"],
-    #     )
-    # )
 
-    # Bearer Token
+    #Basic Auth
+    sheet_response = requests.post(
+        sheet_endpoint,
+        json=sheet_inputs,
+        auth=(
+            os.environ["USERNAME"],
+            os.environ["PASSWORD"],
+        )
+    )
+
+    #Bearer Token
     bearer_headers = {
-        "Authorization": f"Bearer {os.environ['TOKEN']}"
+    "Authorization": f"Bearer {os.environ['TOKEN']}"
     }
     sheet_response = requests.post(
         sheet_endpoint,
