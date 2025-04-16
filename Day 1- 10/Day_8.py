dd93595c3559534a8969fc4b430c679303987d59
D <dagimmengestu5@gmail.com>
2025-04-16 08:49:03 +0300

    Day 39 start

--- Day 1- 10/Day_8.py
+++ Day 1- 10/Day_8.py
@@ -5,7 +5,7 @@ alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
-
+# vbvcbbv
 def caeser(start_text, shift_amount, chiper_direction):
     end_text = ""
     if chiper_direction == "decode":
