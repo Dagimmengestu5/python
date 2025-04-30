c56a77d5394ca8cd77568aaddc9de734be1be962
D <dagimmengestu5@gmail.com>
2025-04-30 15:30:37 +0300

    what

--- Day 1- 10/Day_8.py
+++ Day 1- 10/Day_8.py
@@ -5,7 +5,7 @@ alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#10
+#11
 def caeser(start_text, shift_amount, chiper_direction):
     end_text = ""
     if chiper_direction == "decode":
